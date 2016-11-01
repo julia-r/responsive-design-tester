0c78749f53d3727a49f6bd6bf029d31eaa1b2062
julia-r <julia.rienitz@innogames.de>
2016-11-01 22:24:24 +0100

    initial commit with basic functionality

---
+++ script.js
@@ -0,0 +1,27 @@
+
+new Vue({
+    el: '#app',
+    data: {
+        h_font_size: 18,
+        p_font_size: 14,
+        button_width: 44,
+        button_height: 44,
+        fontStyle: {
+           color: 'red',
+           fontSize: '13px'
+        }
+    },
+    computed: {
+    },
+
+    watch: {
+    },
+    methods: {
+        example: function () {
+
+        },
+    },
+    config: {
+        debug: true
+    }
+});
