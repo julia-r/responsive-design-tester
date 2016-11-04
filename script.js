4b638adcad92a665a5b4c61577b982887cf23c26
Julia Rienitz <julia.rienitz@innogames.com>
2016-11-04 14:11:00 +0100

    Added option to change font-family

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 
-new Vue({
+var vm = new Vue({
     el: '#app',
     data: {
-        h_font_size: 18,
-        p_font_size: 14,
-        button_width: 44,
-        button_height: 44,
+        headline: {
+            font_size: 18,
+            font_family: "Verdana",
+            google_font: false
+        },
+        paragraph: {
+            font_size: 14,
+            font_family: "Verdana",
+            google_font: false
+        },
+        button: {
+            width: 44,
+            height: 44
+        },
         fontStyle: {
            color: 'red',
            fontSize: '13px'
         }
     },
     computed: {
+        headline_font_url: function (){
+            return this.headline.font_family.replace(" ", "+");
+        },
+        paragraph_font_url: function (){
+            return this.paragraph.font_family.replace(" ", "+");
+        },
     },
 
     watch: {
